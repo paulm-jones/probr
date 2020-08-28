d1632b516dd2f75fc764f6b264b7b860328a9c19
David Adams <david.adams@citihub.com>
2020-08-28 19:18:11 +0000

    refactor to expose ProbeState for reuse
    (cherry picked from commit dee2edacb68da1a6646adf4530676cd6e6548d65)

--- test/features/kubernetes/probe/probe.go
+++ test/features/kubernetes/probe/probe.go
@@ -1,8 +1,8 @@
 package probe
 
 import (
-	"github.com/citihub/probr/internal/clouddriver/kubernetes"
-	"github.com/citihub/probr/test/features"
+	"gitlab.com/citihub/probr/internal/clouddriver/kubernetes"
+	"gitlab.com/citihub/probr/test/features"
 	apiv1 "k8s.io/api/core/v1"
 )
 
@@ -13,7 +13,7 @@ type State struct {
 	ExpectedReason *kubernetes.PodCreationErrorReason
 }
 
-// ProcessPodCreationResult is a convenince function to process the result of a pod creation attempt.
+// ProcessPodCreationResult is a convenince function to process the result of a pod creation attempt. 
 // It records state information on the supplied state structure.
 func ProcessPodCreationResult(s *State, pd *apiv1.Pod, expected kubernetes.PodCreationErrorReason, err error) error {
 
@@ -54,7 +54,7 @@ func ProcessPodCreationResult(s *State, pd *apiv1.Pod, expected kubernetes.PodCr
 	return nil
 }
 
-// AssertResult evaluate the state in the context of the expected condition, e.g. if expected is "fail",
+// AssertResult evaluate the state in the context of the expected condition, e.g. if expected is "fail", 
 // then the expecation is that a creation error will be present.
 func AssertResult(s *State, res, msg string) error {
 
