probr/probr-pack-kubernetes

basic webui test

probr.go
@@ -8,8 +8,9 @@ import (
 
 	_ "gitlab.com/citihub/probr/internal/config" //needed for logging
 	"gitlab.com/citihub/probr/test/features"
-	_ "gitlab.com/citihub/probr/test/features/clouddriver"
+	_ "gitlab.com/citihub/probr/test/features/clouddriver"                        //needed to run init on TestHandlers
 	_ "gitlab.com/citihub/probr/test/features/kubernetes/containerregistryaccess" //needed to run init on TestHandlers
+	_ "gitlab.com/citihub/probr/test/features/kubernetes/general"                 //needed to run init on TestHandlers
 	_ "gitlab.com/citihub/probr/test/features/kubernetes/internetaccess"          //needed to run init on TestHandlers
 	_ "gitlab.com/citihub/probr/test/features/kubernetes/podsecuritypolicy"       //needed to run init on TestHandlers
 )
@@ -45,12 +46,14 @@ func RunAllTests() (int, *coreengine.TestStore, error) {
 	addTest(tm, "internet_access", coreengine.Kubernetes, coreengine.InternetAccess)
 	addTest(tm, "pod_security_policy", coreengine.Kubernetes, coreengine.PodSecurityPolicies)
 	addTest(tm, "account_manager", coreengine.CloudDriver, coreengine.General)
+	addTest(tm, "general", coreengine.Kubernetes, coreengine.General)
 
 	//exec 'em all (for now!)
 	s, err := tm.ExecAllTests()
 	return s, tm, err
 }
 
+//GetAllTestResults ...
 func GetAllTestResults(ts *coreengine.TestStore) (map[string]string, error) {
 	out := make(map[string]string)
 	for id := range ts.Tests {
@@ -65,6 +68,7 @@ func GetAllTestResults(ts *coreengine.TestStore) (map[string]string, error) {
 	return out, nil
 }
 
+//ReadTestResults ...
 func ReadTestResults(ts *coreengine.TestStore, id uuid.UUID) (string, string, error) {
 	t, err := ts.GetTest(&id)
 	if err != nil {

test/features/kubernetes/general/general_feature.go
@@ -1,0 +1,97 @@
+package general
+
+import (
+	"strings"
+
+	"gitlab.com/citihub/probr/test/features"
+	"gitlab.com/citihub/probr/test/features/kubernetes/probe"
+
+	"github.com/cucumber/godog"
+	"gitlab.com/citihub/probr/internal/clouddriver/kubernetes"
+	"gitlab.com/citihub/probr/internal/coreengine"
+)
+
+type probeState struct {
+	state probe.State
+}
+
+func init() {
+	td := coreengine.TestDescriptor{Group: coreengine.Kubernetes,
+		Category: coreengine.General, Name: "general"}
+
+	coreengine.TestHandleFunc(td, &coreengine.GoDogTestTuple{
+		Handler: features.GodogTestHandler,
+		Data: &coreengine.GodogTest{
+			TestDescriptor:       &td,
+			TestSuiteInitializer: TestSuiteInitialize,
+			ScenarioInitializer:  ScenarioInitialize,
+		},
+	})
+}
+
+func (p *probeState) aKubernetesClusterIsDeployed() error {
+	b := kubernetes.GetKubeInstance().ClusterIsDeployed()
+
+	if b == nil || !*b {
+		return features.LogAndReturnError("kubernetes cluster is NOT deployed")
+	}
+
+	//else we're good ...
+	return nil
+}
+
+func (p *probeState) iShouldNotBeAbleToAccessTheKubernetesWebUI() error {
+	//TODO: will be difficult to test this.  To access it, a proxy needs to be created:
+	//az aks browse --resource-group rg-probr-all-policies --name ProbrAllPolicies
+	//which will then open a browser at:
+	//http://127.0.0.1:8001/api/v1/namespaces/kube-system/services/https:kubernetes-dashboard:/proxy/#/login
+	//I don't think this is going to be easy to do from here
+	//Is there another test?  Or is it sufficient to verify that no kube-dashboard is running?
+	return nil
+}
+
+func (p *probeState) theKubernetesWebUIIsDisabled() error {
+	//look for the dashboard pod in the kube-system ns
+	pl, err := kubernetes.GetKubeInstance().GetPods("kube-system")
+
+	if err != nil {
+		return features.LogAndReturnError("error raised when trying to retrieve pods %v", err)
+	}
+
+	//a "pass" is the abscence of a "kubernetes-dashboard" pod
+	//if one is found, it's a fail ...
+	for _, p := range pl.Items {
+		if strings.HasPrefix(p.Name, "kubernetes-dashboard") {
+			return features.LogAndReturnError("kubernetes-dashboard pod found (%v) - test fail", p.Name)
+		}
+	}
+
+	//all good if we get to here
+	return nil
+}
+
+//TestSuiteInitialize ...
+func TestSuiteInitialize(ctx *godog.TestSuiteContext) {
+
+	ctx.BeforeSuite(func() {}) //nothing for now
+
+	ctx.AfterSuite(func() {})
+
+}
+
+//ScenarioInitialize ...
+func ScenarioInitialize(ctx *godog.ScenarioContext) {
+	ps := probeState{}
+
+	ctx.BeforeScenario(func(s *godog.Scenario) {
+		features.LogScenarioStart(s)
+	})
+
+	ctx.Step(`^a Kubernetes cluster is deployed$`, ps.aKubernetesClusterIsDeployed)
+	ctx.Step(`^I should not be able to access the Kubernetes Web UI$`, ps.iShouldNotBeAbleToAccessTheKubernetesWebUI)
+	ctx.Step(`^the Kubernetes Web UI is disabled$`, ps.theKubernetesWebUIIsDisabled)
+
+	ctx.AfterScenario(func(s *godog.Scenario, err error) {
+		features.LogScenarioEnd(s)
+	})
+}

paulm-jones/probr

probr.go
@@ -8,8 +8,9 @@ import (
 
 	_ "github.com/citihub/probr/internal/config" //needed for logging
 	"github.com/citihub/probr/test/features"
-	_ "github.com/citihub/probr/test/features/clouddriver"
+	_ "github.com/citihub/probr/test/features/clouddriver"                        //needed to run init on TestHandlers
 	_ "github.com/citihub/probr/test/features/kubernetes/containerregistryaccess" //needed to run init on TestHandlers
+	_ "github.com/citihub/probr/test/features/kubernetes/general"                 //needed to run init on TestHandlers
 	_ "github.com/citihub/probr/test/features/kubernetes/internetaccess"          //needed to run init on TestHandlers
 	_ "github.com/citihub/probr/test/features/kubernetes/podsecuritypolicy"       //needed to run init on TestHandlers
 )
@@ -45,12 +46,14 @@ func RunAllTests() (int, *coreengine.TestStore, error) {
 	addTest(tm, "internet_access", coreengine.Kubernetes, coreengine.InternetAccess)
 	addTest(tm, "pod_security_policy", coreengine.Kubernetes, coreengine.PodSecurityPolicies)
 	addTest(tm, "account_manager", coreengine.CloudDriver, coreengine.General)
+	addTest(tm, "general", coreengine.Kubernetes, coreengine.General)
 
 	//exec 'em all (for now!)
 	s, err := tm.ExecAllTests()
 	return s, tm, err
 }
 
+//GetAllTestResults ...
 func GetAllTestResults(ts *coreengine.TestStore) (map[string]string, error) {
 	out := make(map[string]string)
 	for id := range ts.Tests {
@@ -65,6 +68,7 @@ func GetAllTestResults(ts *coreengine.TestStore) (map[string]string, error) {
 	return out, nil
 }
 
+//ReadTestResults ...
 func ReadTestResults(ts *coreengine.TestStore, id uuid.UUID) (string, string, error) {
 	t, err := ts.GetTest(&id)
 	if err != nil {

test/features/kubernetes/general/general_feature.go
@@ -1,0 +1,97 @@
+package general
+
+import (
+	"strings"
+
+	"github.com/citihub/probr/test/features"
+	"github.com/citihub/probr/test/features/kubernetes/probe"
+
+	"github.com/cucumber/godog"
+	"github.com/citihub/probr/internal/clouddriver/kubernetes"
+	"github.com/citihub/probr/internal/coreengine"
+)
+
+type probeState struct {
+	state probe.State
+}
+
+func init() {
+	td := coreengine.TestDescriptor{Group: coreengine.Kubernetes,
+		Category: coreengine.General, Name: "general"}
+
+	coreengine.TestHandleFunc(td, &coreengine.GoDogTestTuple{
+		Handler: features.GodogTestHandler,
+		Data: &coreengine.GodogTest{
+			TestDescriptor:       &td,
+			TestSuiteInitializer: TestSuiteInitialize,
+			ScenarioInitializer:  ScenarioInitialize,
+		},
+	})
+}
+
+func (p *probeState) aKubernetesClusterIsDeployed() error {
+	b := kubernetes.GetKubeInstance().ClusterIsDeployed()
+
+	if b == nil || !*b {
+		return features.LogAndReturnError("kubernetes cluster is NOT deployed")
+	}
+
+	//else we're good ...
+	return nil
+}
+
+func (p *probeState) iShouldNotBeAbleToAccessTheKubernetesWebUI() error {
+	//TODO: will be difficult to test this.  To access it, a proxy needs to be created:
+	//az aks browse --resource-group rg-probr-all-policies --name ProbrAllPolicies
+	//which will then open a browser at:
+	//http://127.0.0.1:8001/api/v1/namespaces/kube-system/services/https:kubernetes-dashboard:/proxy/#/login
+	//I don't think this is going to be easy to do from here
+	//Is there another test?  Or is it sufficient to verify that no kube-dashboard is running?
+	return nil
+}
+
+func (p *probeState) theKubernetesWebUIIsDisabled() error {
+	//look for the dashboard pod in the kube-system ns
+	pl, err := kubernetes.GetKubeInstance().GetPods("kube-system")
+
+	if err != nil {
+		return features.LogAndReturnError("error raised when trying to retrieve pods %v", err)
+	}
+
+	//a "pass" is the abscence of a "kubernetes-dashboard" pod
+	//if one is found, it's a fail ...
+	for _, p := range pl.Items {
+		if strings.HasPrefix(p.Name, "kubernetes-dashboard") {
+			return features.LogAndReturnError("kubernetes-dashboard pod found (%v) - test fail", p.Name)
+		}
+	}
+
+	//all good if we get to here
+	return nil
+}
+
+//TestSuiteInitialize ...
+func TestSuiteInitialize(ctx *godog.TestSuiteContext) {
+
+	ctx.BeforeSuite(func() {}) //nothing for now
+
+	ctx.AfterSuite(func() {})
+
+}
+
+//ScenarioInitialize ...
+func ScenarioInitialize(ctx *godog.ScenarioContext) {
+	ps := probeState{}
+
+	ctx.BeforeScenario(func(s *godog.Scenario) {
+		features.LogScenarioStart(s)
+	})
+
+	ctx.Step(`^a Kubernetes cluster is deployed$`, ps.aKubernetesClusterIsDeployed)
+	ctx.Step(`^I should not be able to access the Kubernetes Web UI$`, ps.iShouldNotBeAbleToAccessTheKubernetesWebUI)
+	ctx.Step(`^the Kubernetes Web UI is disabled$`, ps.theKubernetesWebUIIsDisabled)
+
+	ctx.AfterScenario(func(s *godog.Scenario, err error) {
+		features.LogScenarioEnd(s)
+	})
+}